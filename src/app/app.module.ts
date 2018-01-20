5ee2c62350f5fd1705a168efd756fbf0ce5aeb80
wackdot <e0167324@u.nus.edu>
2018-01-20 15:34:33 +0800

    20/1/18 3:34 PM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  Routes,
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProtectedComponent } from './protected/protected.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AUTH_PROVIDERS, AuthService } from './auth.service';
+import { LoggedInGuard } from './logged-in.guard';
 
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'protected', component: ProtectedComponent, canActivate: [LoggedInGuard]}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProtectedComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [
+    AUTH_PROVIDERS,
+    LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  message: string;
 
-  ngOnInit() {
+  constructor(public authService: AuthService) { 
+    this.message = '';
   }
 
+  login(username: string, password: string): boolean {
+    this.message = '';
+    if (!this.authService.login(username, password)) {
+      this.message = 'Incorrect credentials.';
+      setTimeout(function() {
+        this.message = '';
+      }.bind(this), 2500);
+    }
+    return false;
+  }
+
+  logout(): boolean {
+    this.authService.logout();
+    return false;
+  }
+  
 }
